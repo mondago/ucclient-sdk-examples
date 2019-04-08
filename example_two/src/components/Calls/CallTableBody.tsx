45e91236ea1cdcf03ae01b9e8693e0fa4dc92aad
Callum Douglas <callum@mondago.com>
2019-04-08 14:54:37 +0100

    change stateless class to function component

--- example_two/src/components/Calls/CallTableBody.tsx
+++ example_two/src/components/Calls/CallTableBody.tsx
@@ -1,19 +1,20 @@
 import * as React from "react"
-import CallController, { CallObject } from "ucsdk-api"
+import UCSession, { CallObject } from "ucclient-sdk"
 import Call from "./Call"
 
 interface CallTableBodyProps {
     calls: { [id: string]: CallObject }
-    callController: CallController
+    callController: UCSession["callController"]
     activeCallId: string
+    updateActiveCall: (id: string) => void
 }
 
-export default class CallTableBody extends React.Component<CallTableBodyProps> {
-    private get _callElements(): React.ReactNode[] {
-        return Object.keys(this.props.calls).map(id => <Call key={id} active={this.props.activeCallId === id} call={this.props.calls[id]} />)
-    }
-
-    render(): React.ReactNode {
-        return <tbody>{this._callElements}</tbody>
-    }
+export default function CallTableBody(props: CallTableBodyProps) {
+    return (
+        <tbody>
+            {Object.keys(props.calls).map(id => (
+                <Call key={id} active={props.activeCallId === id} call={props.calls[id]} updateActiveCall={props.updateActiveCall} />
+            ))}
+        </tbody>
+    )
 }
