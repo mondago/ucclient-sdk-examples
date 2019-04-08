0cdfab8e25f25b6b7c7860f1cf25c6686673bbc3
Callum Douglas <callum@mondago.com>
2019-04-08 15:01:11 +0100

    swap call and called in the table to match headers

--- example_two/src/components/Calls/Call.tsx
+++ example_two/src/components/Calls/Call.tsx
@@ -13,8 +13,8 @@ export default function Call(props: CallProps) {
     return (
         <tr className={"CallRow " + (props.active ? "Active" : "")} onClick={e => props.updateActiveCall(props.call.Id)}>
             <td>{props.call.Id}</td>
-            <td>{props.call.Called.Tel}</td>
             <td>{props.call.Caller.Tel}</td>
+            <td>{props.call.Called.Tel}</td>
             <td>{props.call.State}</td>
         </tr>
     )
